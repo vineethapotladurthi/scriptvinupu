0ac57fa44772c18f2005f775dce6fdc65f62f9e6
vineethapotladurthi <vineethapotladurthi71@gmail.com>
2026-01-19 18:06:13 +0530

    updated successfully...

--- conditionalstatements.sh
+++ conditionalstatements.sh
@@ -3,7 +3,7 @@
 #definning variables
 HOSTNAME=$(hostname)
 
-echo " our server name is: $HOSTNAME"
+echo " our server name is:it $HOSTNAME"
 PWD=$(pwd)
-echo " the path is: ${PWD}"
+echo " the path is: $PWD"
 echo "helo"
