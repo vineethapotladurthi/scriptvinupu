1513048fbb3a4f3d47646c1a47a2ed47a4704bc3
vineethapotladurthi <vineethapotladurthi71@gmail.com>
2026-01-22 14:55:22 +0530

    installation process successs

--- installations.sh
+++ installations.sh
@@ -19,3 +19,20 @@ if [ $? -ne 0 ]; then
 else
     echo " git was already iinstalled"
 fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+ then
+    echo " mysql was not installed on your device let us install mysql"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+     then
+        echo "mysql installation was not success"
+        exit 1
+    else
+        echo " mysql installed succcessfully"
+    fi
+else
+    echo " mysql was already iinstalled"
+fi
